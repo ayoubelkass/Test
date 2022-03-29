5b162901df39a37f9b1db845ed3155576a84fe42
ayoubelkass <81431025+ayoubelkass@users.noreply.github.com>
2022-03-29 11:30:23 +0000

    Update Main.java
    ok

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, ayoub!");
+        System.out.println("C'est une modification pour déclencher un trigger sur JENKINS");
     }
 }
 
